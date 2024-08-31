7c5212ad944b75c19a0ceaf078dfdbf331a8dd05
doseung <didehtmd@gmail.com>
2024-08-31 19:13:43 +0900

    first snippet

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import UsingIconWithCSSClass from "./pages/UsingIconWithCSSClass";
+import UsingIcon from "./pages/UsingIcon";
+import Style from "./pages/Style";
+import Icon from "./pages/Icon";
+import Bootstrap from "./pages/Bootstrap";
+
+export default function App() {
+  return (
+    <div>
+      <UsingIconWithCSSClass />
+      <UsingIcon />
+      <Style />
+      <Icon />
+      <Bootstrap />
+    </div>
+  );
+}
