21c7a8f38c01878065f94b3e890a33e98dda0a64
doseung <didehtmd@gmail.com>
2024-09-01 02:59:52 +0900

    material css import

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Bootstrap 로그인 창 구현을 위해 구현하였습니다.
 
 - material-icons 을 통해서 아이콘을 구성합니다.
-
-# 서브 스니펫을 위해서 react + typescript로 구성하였습니다.
+- 서브 스니펫을 위해서 react + typescript로 구성하였습니다.
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,13 +1,15 @@
-import type {FC, CSSProperties} from 'react'
+import type {FC, DetailedHTMLProps, HTMLAttributes} from 'react'
 
-export type IconProps = {
+type ReactSpanProps = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
+
+export type IconProps = ReactSpanProps & {
   name: string
-  style?: CSSProperties
 }
 
-export const Icon: FC<IconProps> = ({name, ...props}) => {
+export const Icon: FC<IconProps> = ({name, className: _className, ...props}) => {
+  const className = ['material-icons', _className].join(' ')
   return (
-    <span {...props} className="material-icons">
+    <span {...props} className={className}>
       {name}
     </span>
   )
